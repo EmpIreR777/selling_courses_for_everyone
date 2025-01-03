ad3449567c17d940dab888e5bdfad898111f8adc
TANATOS <Kirill.tanatos1993@mail.ru>
2025-01-04 01:42:50 +0300

    fix

--- bot/migration/env.py
+++ bot/migration/env.py
@@ -14,7 +14,7 @@ from bot.dao.models import Product, Purchase, User, Category
 
 
 config = context.config
-config.set_main_option("sqlalchemy.url", database_url)
+config.set_main_option('sqlalchemy.url', database_url)
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
